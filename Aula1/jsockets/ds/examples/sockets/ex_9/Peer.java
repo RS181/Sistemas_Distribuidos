a2b3f5432179b995cff5d5cd3fcdcc3075f75213
RS181 <ruisantos.18181@gmail.com>
2024-10-31 16:01:11 +0000

    primeira tentativa de merge de set's (com set fixo)

--- Aula1/jsockets/ds/examples/sockets/ex_9/Peer.java
+++ Aula1/jsockets/ds/examples/sockets/ex_9/Peer.java
@@ -7,24 +7,25 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
-
 
 public class Peer {
 	String host;
 	int port;
 	Logger logger;
 
-
-	public Peer(String hostname,String port) {
+	public Peer(String hostname, String port) {
 		host = hostname;
 		this.port = Integer.parseInt(port);
 		logger = Logger.getLogger("logfile");
 		try {
-			FileHandler handler = new FileHandler("./" + hostname + "_peer_" + port +".log", false);
+			FileHandler handler = new FileHandler("./" + hostname + "_peer_" + port + ".log", false);
 			logger.addHandler(handler);
 			SimpleFormatter formatter = new SimpleFormatter();
 			handler.setFormatter(formatter);
@@ -34,39 +35,55 @@ public class Peer {
 	}
 
 	public static void main(String[] args) throws Exception {
-		Peer peer = new Peer(args[0],args[1]);
+		Peer peer = new Peer(args[0], args[1]);
 		System.out.printf("new peer @ host=%s\n", args[0]);
 
-		//Start server thread
-		Server server =new Server(args[0], Integer.parseInt(args[1]), peer.logger); 
-
+		// Start server thread
+		Server server = new Server(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), peer.logger);
 		new Thread(server).start();
 
-		//Start SyncronizedRequest
-		SyncronizedRequest syncronizedRequest = new SyncronizedRequest(peer.host,peer.port, Integer.parseInt(args[2]), peer.logger);
+		// Start SyncronizedRequest
+		SyncronizedRequest syncronizedRequest = new SyncronizedRequest(peer.host, peer.port, Integer.parseInt(args[2]),
+				peer.logger);
 		new Thread(syncronizedRequest).start();
 
-		
-
+		// S
 
 	}
 }
 
-
 /**
- * Deals with Syncronization requests 
+ * Deals with Syncronization requests
  */
 class Server implements Runnable {
+	// Atributes of current peer
 	String host;
 	int port;
 	ServerSocket server;
 	Logger logger;
+	Set<Integer> data = new HashSet<>(); // Local Set of data that we want to syncronize
 
-	public Server(String host, int port, Logger logger) throws Exception {
+	// Atributes of Peer we are going to connect
+	String nextHost = "localhost"; // assuming we are using localhost
+	int nextPort;
+
+	public Server(String host, int port, int nextPort, Logger logger) throws Exception {
 		this.host = host;
 		this.port = port;
+		this.nextPort = nextPort;
 		this.logger = logger;
 		server = new ServerSocket(port, 1, InetAddress.getByName(host));
+
+		// Comentar depois (so usei para testar)
+		if (port == 22222) {
+			data.addAll(Arrays.asList(new Integer[] { 1, 3, 5, 7, 9 }));
+		} else {
+			data.addAll(Arrays.asList(new Integer[] { 0, 2, 4, 6, 8 }));
+		}
+	}
+
+	public void addNumberToData(int n) {
+		data.add(n);
 	}
 
 	@Override
@@ -75,22 +92,39 @@ class Server implements Runnable {
 			logger.info("server: endpoint running at port " + port + " ...");
 			while (true) {
 				try {
-					//Waits for syncronization request
+					// Waits for syncronization request
 					Socket client = server.accept();
 					String clientAddress = client.getInetAddress().getHostAddress();
-					
+
 					BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    String request = in.readLine(); // Recebe o pedido 
+					String request = in.readLine(); // Recebe o pedido
 
-					if (isSyncronizationRequest(request)){
-						int senderPort = getOriginPort(request);
+					synchronized (data) {
 
-						logger.info("Server received SYNCRONIZATION request from " + clientAddress + " @" +senderPort);
+						if (isSyncronizationRequest(request)) {
+							int senderPort = getOriginPort(request);
+
+							logger.info(
+									"Server: received SYNCRONIZATION request from " + clientAddress + " @"
+											+ senderPort);
+
+							// do the merge of sets (todo:Verificar se esta e a forma pretendida)
+							sendData();
+
+						} else { // Receive numbers and put them in data Set
+							int n = Integer.parseInt(request);
+
+							if (!data.contains(n)) {
+								logger.info("Server: Adding [" + n + "] to local data Set");
+								addNumberToData(Integer.parseInt(request));
+							} 
+							// else {
+							// logger.info("Server: The number [" + n + "] already exists in local data Set");
+							// }
+
+						}
 					}
-					
-					
 
-					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -100,27 +134,58 @@ class Server implements Runnable {
 		}
 	}
 
+	private void sendData() {
+		try {
+
+			for (int n : data) {
+				/*
+				 * make connection
+				 */
+				Socket socket = new Socket(nextHost, nextPort);
+
+				/*
+				 * prepare socket output channel
+				 */
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				/*
+				 * Send number n
+				 */
+				logger.info("Server: sending number [" + n + "] to @" + nextPort);
+				out.println(n);
+				out.flush();
+				/*
+				 * close connection
+				 */
+				socket.close();
+			}
+
+		} catch (Exception e) {
+			logger.info("Server: error ocurred while triing to send info to Peer " + nextHost + " @" + nextPort);
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Checks if a string is a Syncronization Request
 	 * (basicaly checks if request contains the string "SYNC-DATA")
+	 * 
 	 * @param request
 	 * @return
 	 */
-	private Boolean isSyncronizationRequest(String request){
+	private Boolean isSyncronizationRequest(String request) {
 		return request.contains("SYNC-DATA");
 	}
 
 	/**
 	 * 
-	 * @param request  SYNC-DATA:port_of_sender
+	 * @param request SYNC-DATA:port_of_sender
 	 * @return port that originated this request
 	 */
-	private int getOriginPort(String request){
+	private int getOriginPort(String request) {
 		Scanner sc = new Scanner(request).useDelimiter(":");
 		sc.next();
 		String ans = sc.next();
-		
+
 		return Integer.parseInt(ans);
 
 	}
