e10dcbdbca6555e1ac726578028077beb3e48e0f
RS181 <ruisantos.18181@gmail.com>
2024-12-18 12:12:23 +0000

    classe que representa relogios de lampart

---
+++ ds/assign/tom/LamportClock.java
@@ -0,0 +1,28 @@
+package ds.assign.tom;
+
+public class LamportClock {
+
+    private int time = 0;
+
+    public LamportClock(int time){
+        this.time = time;
+    }
+
+
+    public int getTime() {
+        return time;
+    }
+
+
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+
+    public void increment(){
+        this.time++;
+    }
+
+    
+}
